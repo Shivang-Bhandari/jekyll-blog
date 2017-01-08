f5bd3519e8b4317ac030d26bf68935ed0cd7fbd8
Shivang-Bhandari <pump382156@gmail.com>
2017-01-09 04:46:27 +0530

    Gulp Fixed and DevDependencies Updated

--- gulpfile.js
+++ gulpfile.js
@@ -1,37 +1,47 @@
-const gulp=require('gulp');
-const child = require('child_process');
-const gutil = require('gulp-util');
+var gulp        = require('gulp');
+var cp          = require('child_process');
+var browserSync = require('browser-sync').create();
 
-const browserSync = require('browser-sync').create();
-const siteRoot = '_site';
-
-gulp.task('jekyllServe', () => {
-  const jekyll = child.spawn('jekyll', ['build',
-    '--watch',
-    '--incremental',
-    '--drafts'
-  ]);
-
-  const jekyllLogger = (buffer) => {
-    buffer.toString()
-      .split(/\n/)
-      .forEach((message) => gutil.log('Jekyll: ' + message));
-  };
-
-  jekyll.stdout.on('data', jekyllLogger);
-  jekyll.stderr.on('data', jekyllLogger);
+/**
+ * Build the Jekyll Site
+ */
+gulp.task('jekyll', function(done) {
+  browserSync.notify('Compiling Jekyll');
+  var jekyll_command = ['exec', 'jekyll', 'build', '--watch'];
+  return cp.spawn('bundle', jekyll_command, {
+    stdio: 'inherit'
+  }).on('close', done);
 });
 
+// gulp.task('jekyllServe', function(){
+//   browserSync.notify('Compiling Jekyll');
+//   var jekyll_command = ['exec', 'jekyll', 'serve', '--watch'];
+//   return cp.spawn('bundle', jekyll_command, {
+//     stdio: 'inherit'
+//   }).on('close', done);
+// });
 
-gulp.task('browserSync', () => {
-   browserSync.init({
-     files: [siteRoot + '/**'],
-     port: 4000,
-     server: {
-       baseDir: siteRoot
-     }
-   });
-  gulp.watch();
- });
 
-gulp.task('default', ['jekyllServe', 'browserSync']);
+gulp.task('serve', function() {
+  browserSync.init({
+       files: ['_site' + '/**'],
+       port: 5000,
+       server: {
+         baseDir: '_site'
+       }
+     });
+
+     gulp.watch("/static/css/blog.css", ['jekyll-rebuild']);
+     gulp.watch("/*.html", ['jekyll-rebuild']);
+     gulp.watch("/_layouts/*.html", ['jekyll-rebuild']);
+     gulp.watch("/_includes/*.html", ['jekyll-rebuild']);
+});
+
+/**
+ * Reload the Browser when the build is completed.
+ */
+gulp.task('jekyll-rebuild', ['jekyll'], function() {
+  browserSync.reload();
+});
+
+gulp.task('default', ['jekyll', 'serve']);
